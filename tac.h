bfc23433343db89c47518626e0e31ccd21aa36d7
Martin Buchta <martin.buchta@delamewww.cz>
2018-11-11 13:38:04 +0100

    Created three-address code generator skeleton.
    Just created empty tac.c and tac.h files.

---
+++ tac.h
@@ -0,0 +1,22 @@
+/*
+ *	Project: IFJ18
+ *	Course: Formal Languages and Compilers
+ *	School: Brno University of Technology
+ *
+ *	Authors:
+ *	- Demel Jan <xdemel01@stud.fit.vutbr.cz>
+ *	- Sedláček Aleš	<xsedla1c@stud.fit.vutbr.cz>
+ *	- Buchta Martin	<xbucht28@stud.fit.vutbr.cz>
+ *	- Hruška Jozef <xhrusk25@stud.fit.vutbr.cz>
+ *
+ *	May the force be with you.
+ */
+
+/*
+ * Three-address code generator
+ */
+
+#ifndef IFJ_PROJECT_TAC_H
+#define IFJ_PROJECT_TAC_H
+
+#endif //IFJ_PROJECT_TAC_H
