f0143735e2aedb8669ab479583d1964f3b899930
Martin Buchta <martin.buchta@delamewww.cz>
2018-11-16 21:04:23 +0100

    Created data structures
    tData, tDataType, dDataValue

--- semantic.h
+++ semantic.h
@@ -16,6 +16,32 @@
 #define IFJ_PROJECT_SEMANTIC_H
 
 /**
+ * @brief Union representing any data
+ */
+typedef union {
+    int i;
+    float f;
+    char *s;
+} tDataValue;
+
+/**
+ * @brief Types of any data enumeration
+ */
+typedef enum {
+    INTEGER,
+    FLOAT,
+    STRING
+} tDataType;
+
+/**
+ * @brief Struct representing any data and it's type
+ */
+typedef struct {
+    tDataValue value;
+    tDataType dataType;
+} tData;
+
+/**
  * @brief Creates new global table
  */
 void initGlobalSymTable();
