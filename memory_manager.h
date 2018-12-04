3e07759cac247c67ecd10e9d5013c9aac99eae9a
Jan Demel <jandemel98@gmail.com>
2018-12-05 00:16:40 +0100

    Memory manager init

---
+++ memory_manager.h
@@ -0,0 +1,12 @@
+#ifndef __memory_manager
+#define __memory_manager
+
+#include "scanner.h"
+
+/**
+ * @brief Frees token
+ * @param token Token to free
+ */
+void free_token(sToken *token);
+
+#endif
