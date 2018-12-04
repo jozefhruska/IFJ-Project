e9ac26ab39c99ec47eb884503fab29ed133bd1ee
Jan Demel <jandemel98@gmail.com>
2018-12-04 19:26:20 +0100

    Added tests

--- build_tests.sh
+++ build_tests.sh
@@ -66,6 +66,50 @@ then
     
 
     echo
-    echo "====================================== SHOULD FAIL ======================================"
-    #./demel test_programms/f_01
+    echo "=================================== SHOULD FAIL (ERRCODE = 1) ======================================"
+    echo
+    ./demel advanced_tests/return_value_1/1.rb
+    ./demel advanced_tests/return_value_1/2.rb
+    ./demel advanced_tests/return_value_1/3.rb
+    ./demel advanced_tests/return_value_1/4.rb
+    ./demel advanced_tests/return_value_1/5.rb
+
+    echo
+    echo "=================================== SHOULD FAIL (ERRCODE = 2) ======================================"
+    echo
+    ./demel advanced_tests/return_value_2/1.rb
+    ./demel advanced_tests/return_value_2/2.rb
+    ./demel advanced_tests/return_value_2/3.rb
+    ./demel advanced_tests/return_value_2/identifs.rb
+
+    echo
+    echo "=================================== SHOULD FAIL (ERRCODE = 3) ======================================"
+    echo
+    ./demel advanced_tests/return_value_3/1.rb
+    ./demel advanced_tests/return_value_3/2.rb
+    ./demel advanced_tests/return_value_3/3.rb
+    ./demel advanced_tests/return_value_3/4.rb
+    ./demel advanced_tests/return_value_3/5.rb
+    ./demel advanced_tests/return_value_3/6.rb
+    ./demel advanced_tests/return_value_3/7.rb
+    ./demel advanced_tests/return_value_3/8.rb
+    ./demel advanced_tests/return_value_3/9.rb
+    ./demel advanced_tests/return_value_3/10.rb
+    ./demel advanced_tests/return_value_3/11.rb
+    ./demel advanced_tests/return_value_3/12.rb
+    ./demel advanced_tests/return_value_3/13.rb
+    ./demel advanced_tests/return_value_3/14.rb
+    ./demel advanced_tests/return_value_3/15.rb
+    ./demel advanced_tests/return_value_3/16.rb
+    ./demel advanced_tests/return_value_3/17.rb
+    ./demel advanced_tests/return_value_3/olok.rb
+
+    echo
+    echo "=================================== SHOULD FAIL (ERRCODE = 5) ======================================"
+    echo
+    ./demel advanced_tests/return_value_5/1.rb
+    ./demel advanced_tests/return_value_5/2.rb
+    ./demel advanced_tests/return_value_5/3.rb
+    ./demel advanced_tests/return_value_5/4.rb
+    ./demel advanced_tests/return_value_5/5.rb
 fi
